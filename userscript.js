d162097e70b3d95ca4a86c965db5c7fb56860c43
Russell Craigie <iammp6@gmail.com>
2020-04-25 12:14:14 -0700

    Update userscript settings

--- userscript.js
+++ userscript.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         Mp6's Cookie Addons
 // @source       https://github.com/Mp6/clickeraddons
-// @version      0.5
+// @version      0.6
 // @updateURL    https://raw.githubusercontent.com/Mp6/clickeraddons/master/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/Mp6/clickeraddons/master/userscript.js
 // @description  Minor addons for cookie clicker
 // @author       Mp6
 // @match        https://orteil.dashnet.org/cookieclicker/
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 function Mp6LoadMod() {
+	console.log('Loading Mods');
 	Game.LoadMod('https://raw.githubusercontent.com/Mp6/clickeraddons/master/initialize_scripts.js');
 }
 
-window.addEventListener("ready", Mp6LoadMod, false);
+window.addEventListener("load", Mp6LoadMod, false);
